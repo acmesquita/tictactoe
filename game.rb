6e0d3aabcf9022a6938cf34150c2095ea5e799d4
Catharina Mesquita <catha.ana.1994@gmail.com>
2018-05-20 16:29:02 -0300

    Adicionando o nível fácil

--- game.rb
+++ game.rb
@@ -1,8 +1,9 @@
 class Game
     def initialize(options=[])
       @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-      @com = "X" # the computer's marker
-      @hum = "O" # the user's marker
+      @player2 = "X" # the computer's marker
+      @player1 = "O" # the user's marker
+      @modality = options[:modality]
     end
   
     def start_game
@@ -12,7 +13,7 @@ class Game
       until game_is_over(@board) || tie(@board)
         get_human_spot
         if !game_is_over(@board) && !tie(@board)
-          eval_board
+          eval_board(@modality)
         end
         draw_board(@board)
       end
@@ -24,7 +25,7 @@ class Game
       until spot
         spot = get_spot;
         if @board[spot] != "X" && @board[spot] != "O"
-          @board[spot] = @hum
+          @board[spot] = @player1
         else
           spot = nil
         end
@@ -32,37 +33,59 @@ class Game
     end
 
     def get_spot
-        begin
-            value = Integer(gets.chomp)
-            if(value.between?(0,8))
-                return value
-            else 
-                puts "Input invalid. Enter [0-8]:"
-                get_spot
-            end
-        rescue ArgumentError
-            puts "Intup invalid. Enter [0-8]:"
+      begin
+        value = Integer(gets.chomp)
+        if(value.between?(0,8))
+            return value
+        else 
+            puts "Input invalid. Enter [0-8]:"
             get_spot
         end
+      rescue ArgumentError
+        puts "Intup invalid. Enter [0-8]:"
+        get_spot
+      end
     end
   
-    def eval_board
+    def eval_board(modality)
       spot = nil
       until spot
         if @board[4] == "4"
           spot = 4
-          @board[spot] = @com
+          @board[spot] = @player2
         else
-          spot = get_best_move(@board, @com)
+          spot = get_move(modality, @board, @player2)
           if @board[spot] != "X" && @board[spot] != "O"
-            @board[spot] = @com
+            @board[spot] = @player2
           else
             spot = nil
           end
         end
       end
     end
+
+    def get_move(modality, board, next_player)
+      case modality
+      when 1
+        return get_easy_move(board, next_player)
+      when 2
+        return get_best_move(board, next_player)
+      else
+        return get_easy_move(board, next_player)
+      end
+    end
   
+    def get_easy_move(board, next_player)
+      available_spaces = []
+      board.each do |s|
+          if s != "X" && s != "O"
+              available_spaces << s
+          end
+      end
+      n = rand(0..available_spaces.count)
+      available_spaces[n].to_i
+    end
+
     def get_best_move(board, next_player, depth = 0, best_score = {})
       available_spaces = []
       best_move = nil
@@ -72,13 +95,13 @@ class Game
         end
       end
       available_spaces.each do |as|
-        board[as.to_i] = @com
+        board[as.to_i] = @player2
         if game_is_over(board)
           best_move = as.to_i
           board[as.to_i] = as
           return best_move
         else
-          board[as.to_i] = @hum
+          board[as.to_i] = @player1
           if game_is_over(board)
             best_move = as.to_i
             board[as.to_i] = as
@@ -113,9 +136,9 @@ class Game
     end
 
     def draw_board(b)
-        system("clear")
-        puts " #{b[0]} | #{b[1]} | #{b[2]} \n===+===+===\n #{b[3]} | #{b[4]} | #{b[5]} \n===+===+===\n #{b[6]} | #{b[7]} | #{b[8]} \n"
-        puts "Enter [0-8]:"
+      system("clear")
+      puts " #{b[0]} | #{b[1]} | #{b[2]} \n===+===+===\n #{b[3]} | #{b[4]} | #{b[5]} \n===+===+===\n #{b[6]} | #{b[7]} | #{b[8]} \n"
+      puts "Enter [0-8]:"
     end
   
   end  
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,18 @@
 load('game.rb')
-game = Game.new
+
+modality = 1;
+
+begin
+  puts "Modality [1-easy, 2-hard]:"
+  value = Integer(gets.chomp)
+  if(value.between?(1,2))
+    modality = value
+  else 
+    puts "Input invalid. Start again 1"
+    exit
+  end
+  rescue ArgumentError
+end
+
+game = Game.new({:modality=> modality})
 game.start_game
